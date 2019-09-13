0234d1c462b724fce6b00244e8927838ed9fe009
Nate Barlow <n8barlow@gmail.com>
2019-09-13 02:42:46 -0600

    Lose the table layout

--- index.php
+++ index.php
@@ -6,55 +6,31 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Meme generator</title>
+<style>
+	body { text-align: center; font-size:18px; }
+	img { min-width: 384px; }
+	input[type="text"]  { width:384px; font-size:inherit; }
+	button { width:384px; font-size:inherit; margin: 0 4px; }
+	#Share { display:none; }
+</style>
 </head>
 
 <body>
-
-    <div style="width:40%;margin:30px auto;" >
-        <table>
-            <tr>
-                <td colspan="2" align="center">
-                    <img src="testing.jpg" id="gag" />
-                </td>
-            </tr>
-            <tr>
-                <td>
-                	<input type="text" placeholder="Upper Text" id="upperTextbox"/>	
-                </td>
-                <td>
-                	<input type="text" placeholder="Lower Text" id="lowerTextbox"/>	
-                </td>
-            </tr>
-            <tr>
-            	<!-- td><p style="font-size:14px;color:#CCC;">Image updates on textbox focus out</p></td -->
-				<td colspan="2" align="center">
-					<button id="Generate" onClick="memeIt();"><big>Go!</big></button>
-				</td>
-            </tr>
-        </table>
-    </div>
-<div id="error"></div>
+	
+<img src="testing.jpg" id="gag"/> <br/><br/>
+<input type="text" placeholder="Upper Text" id="upperTextbox" /> <br/><br/>
+<input type="text" placeholder="Lower Text" id="lowerTextbox" /> <br/><br/>
+<button id="Generate" onClick="memeIt();" >Go!</button> <br/><br/>
+<button id="Share" >Share!</button>
+<p id="error"></p>
+	
 </body>
 </html>
 <script src="jquery.js" type="text/javascript" ></script>
 <script>
-
-	/*
-	$('#lowerTextbox').focusout(function(){
-		if($.trim($(this).val()) != "")
-		{
-			SendAjaxRequest($('#upperTextbox').val(),$(this).val(),"lower");
-		}
-	});
-	$('#upperTextbox').focusout(function(){
-		if($.trim($(this).val()) != "")
-		{
-			SendAjaxRequest($(this).val(),$('#lowerTextbox').val(),"upper");
-		}
-	});
-	/**/
-	
-	function memeIt() {
+	var response;
+	function memeIt() 
+	{
 		SendAjaxRequest( $('#upperTextbox').val(), $('#lowerTextbox').val());
 	}
 	
@@ -66,12 +42,17 @@
 			data: { upmsg : umsg, downmsg : dmsg, position: type },
 			success:function(e)
 			{
-				console.log(e);
-				var response=JSON.parse(e);
+				//console.log(e);
+				response=JSON.parse(e);
 				//console.log(response);
 				//$('#error').html(e);
 				var src= response.imageFolder+'/'+response.imageFile+'?c='+ Math.random().toString(36).substring(2, 15);
 				$('#gag').attr('src',src);
+				
+				$('#Share').click( function() {
+						
+				});
+				//$('#Share').css('display','inline');
 			}
 		});
 	}
